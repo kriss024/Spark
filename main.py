18658b91edba6dbcf03e2b791f45a00aed7ae453
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2025-09-15 10:03:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,20 @@ import os
 path="/home/Work/Spark"
 os.chdir(path)
 
-os.system("pyspark --name SparkApp --master local[*] --driver-memory 4G --executor-memory 4G --conf spark.sql.catalogImplementation=hive")
+os.system("pyspark3 --master yarn \
+--name=SparkApp \
+--queue=SparkQueue \
+--driver-memory=16G \
+--num-executors=6 \
+--executor-memory=16G \
+--executor-cores=4 \
+--conf 'spark.sql.catalogImplementation=hive' \
+--conf 'spark.sql.adaptive.enabled=true' \
+--conf 'spark.dynamicAllocation.enabled=true' \
+--conf 'spark.dynamicAllocation.maxExecutors=50' \
+--conf 'spark.sql.codegen.wholeStage=true' \
+--conf 'spark.sql.inMemoryColumnarStorage.compressed=true' \
+")
+
+spark.sparkContext.setLogLevel("ERROR")
+spark.sparkContext.setCheckpointDir('hdfs://.../tmp')
